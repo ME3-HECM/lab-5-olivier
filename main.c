39a95c2b0e2889c58999c93d3aaefa46c40a3eaf
Olivermcn <oliver.mcnaughton@gmail.com>
2023-02-12 14:51:13 +0000

    init 2

--- main.c
+++ main.c
@@ -9,5 +9,15 @@
 #include <xc.h>
 
 void main(void) {
+    RC0PPS = 0x12; // Map EUSART4 TX to RC0
+    RX4PPS = 0x11; // RX is RC1   
+    BAUD4CONbits.BRG16 = 0; 	//set baud rate scaling
+    TX4STAbits.BRGH = 0; 		//high baud rate select bit
+    SP4BRGL = 103; 			//set baud rate to 103 = 9600bps
+    SP4BRGH = 0;			//not used
+
+    RC4STAbits.CREN = 1; 		//enable continuos reception
+    TX4STAbits.TXEN = 1; 		//enable transmitter
+    RC4STAbits.SPEN = 1; 		//enable serial port
     return;
 }
