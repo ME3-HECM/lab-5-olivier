3deba8d3edaec73c2fe767ce3d2fb2a25f582eb5
wisdomokoro1232 <122476723+wisdomokoro1232@users.noreply.github.com>
2023-02-14 11:18:08 +0000

    EX1.1 Print character to LCD
    Printing character to LCD from Realterm

--- main.c
+++ main.c
@@ -5,15 +5,22 @@
  * Created on 12 February 2023, 14:49
  */
 
+#pragma config FEXTOSC = HS     // External Oscillator mode Selection bits (HS (crystal oscillator) above 8 MHz; PFM set to high power)
+#pragma config RSTOSC = EXTOSC_4PLL// Power-up default value for COSC bits (EXTOSC with 4x PLL, with EXTOSC operating per FEXTOSC bits)
+#pragma config WDTE = OFF        // WDT operating mode (WDT enabled regardless of sleep)
 
 #include <xc.h>
 #include "serial.h"
 #include "LCD.h"
+#include <stdio.h>
 
+#define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
 void main(void) {
-    char data;
+    char *data;
     initUSART4();
     LCD_Init();
-    data= getCharSerial4();
-    LCD_sendstring(data);
+    LCD_setline(0);
+    while (1){
+    LCD_sendbyte(getCharSerial4(),1);
+}
 }
